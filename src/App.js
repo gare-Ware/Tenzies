42c2dc9165b750d2cd40d73217e7597615d1d666
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-02-18 11:58:46 -0500

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
+  
+  const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [rolls, setRolls] = React.useState(0)
+    const [seconds, setSeconds] = React.useState(0)
+    const [highScore, setHighScore] = React.useState(
+        () => JSON.parse(localStorage.getItem('highScore')) || { rolls: 0, time: 0 }
+    )
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+        }
+    }, [dice])
+    
+    React.useEffect(() => {
+        let interval
+        if(!tenzies){
+            interval = setInterval(() => {
+                setSeconds(prevSeconds => prevSeconds + 1)
+            }, 1000)
+        } else {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval)
+    }, [seconds])
+    
+    React.useEffect(() => {
+        localStorage.setItem('highScore', JSON.stringify(highScore))
+    }, [highScore])
+    
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+            setRolls(prevRolls => prevRolls + 1)
+        } else {
+            checkHighScore(rolls, seconds)
+            setTenzies(false)
+            setRolls(0)
+            setSeconds(0)
+            setDice(allNewDice())
+        }
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    function checkHighScore(rolls, seconds) {
+        if(seconds < highScore.time || highScore.time === 0){
+            const newHighScore = {
+                rolls: rolls,
+                time: seconds
+            }
+            setHighScore(newHighScore)
+        }
+    }
+
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      {tenzies && <Confetti tweenDuration="1" />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. 
+      Click each die to freeze it at its current value between rolls.</p>
+      <div className="dice-container">
+          {diceElements}
+      </div>
+      <div className='tracker-container'>
+          <p className='tracker'>Rolls: {rolls}</p>
+          <p className='tracker'>{seconds}s</p>
+      </div>
+      <button 
+          className="roll-dice" 
+          onClick={rollDice}
+      >
+          {tenzies ? "New Game" : "Roll"}
+      </button>
+      {highScore.time !== 0 && <p className='high-score'>High Score: {highScore.rolls} rolls | {highScore.time}s</p>}
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Die.js
@@ -0,0 +1,82 @@
+import React from "react"
+import {nanoid} from "nanoid"
+
+export default function Die(props) {
+    
+    function dotGenerator(num) {
+        let dots = []
+        for(let i=0; i<[num]; i++){
+            dots.push(<div key={nanoid()} className='dot'></div>)
+        }
+        return dots
+    }
+    
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    
+    let dieHtml
+
+    if(props.value === 1){
+        dieHtml =     
+            <div className='die-face one'>
+                {dotGenerator(1)}
+            </div>
+    }
+    if(props.value === 2){
+        dieHtml =     
+            <div className='die-face two'>
+                {dotGenerator(2)}
+            </div>
+    }
+    if(props.value === 3){
+        dieHtml =     
+            <div className='die-face three'>
+                {dotGenerator(3)}
+            </div>
+    }
+    if(props.value === 4){
+        dieHtml =     
+            <div className='die-face'>
+                <div className='dot-container'>
+                    {dotGenerator(2)}
+                </div>
+                <div className='dot-container'>
+                    {dotGenerator(2)}
+                </div>
+            </div>
+    }
+    if(props.value === 5){
+        dieHtml =     
+            <div className='die-face'>
+                <div className='dot-container'>
+                    {dotGenerator(2)}
+                </div>
+                {dotGenerator(1)}
+                <div className='dot-container'>
+                    {dotGenerator(2)}
+                </div>
+            </div>
+    }
+    if(props.value === 6){
+        dieHtml =     
+            <div className='die-face'>
+                <div className='dot-container'>
+                    {dotGenerator(3)}
+                </div>
+                <div className='dot-container'>
+                    {dotGenerator(3)}
+                </div>
+            </div>
+    }
+    
+    return (
+        <div  
+            className='die-container'
+            style={styles}
+            onClick={props.holdDice}
+        >
+            {dieHtml}
+        </div>
+    )
+}
